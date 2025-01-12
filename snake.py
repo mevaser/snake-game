36442b8bcbf3dd6e0023bf5a41d1a766007aa335
Mevaser Zehoray <Mevaserruppin@gmail.com>
2025-01-12 08:19:40 +0200

    Refactor Snake Game to OOP structure

---
+++ snake.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+        self.create_snake()
+
+    def create_snake(self):
+        for position in self.starting_positions:
+            new_segment = Turtle(shape="square")
+            new_segment.penup()
+            new_segment.goto(position)
+            new_segment.color("white")
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
